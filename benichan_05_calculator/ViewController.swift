bdf61ab57c09be9f358f73a92908b878f2d34c68
YuKono <cosmos17128@gmail.com>
2021-05-12 14:33:40 +0900

    電卓途中まで

--- benichan_05_calculator/ViewController.swift
+++ benichan_05_calculator/ViewController.swift
@@ -8,7 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    //計算結果を表示するラベル
+    @IBOutlet var label:UILabel!
+    var number1:Int = 0
+    var number2:Int = 0
+    var number3:Int = 0
+    //どの四則演算をするか判定する変数
+    var ope:Int = 0
 
+    @IBAction func select3(){
+        number1 = number1*10 + 3
+        label.text = String(number1) //String型にキャストして代入
+    }
+    
+    @IBAction func select4(){
+        number1 = number1*10 + 4
+        label.text = String(number1) //String型にキャストして代入
+    }
+    
+    @IBAction func plus(){
+        number2 = number1
+        number1 = 0
+    }
+    
+    @IBAction func equal(){
+        number3 = number1 + number2
+        label.text = String(number3)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
